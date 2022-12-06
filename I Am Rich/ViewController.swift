0e9f3813da5227903db72254d68a818e26178ac0
Georgi Sirakov <georgi.sirakov74@gmail.com>
2022-12-06 11:27:04 +0200

    Creating App icons via Canva, creating two images with the main three sizes

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Georgi Sirakov on 5.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func toggleButton(_ sender: UIButton) {
+        
+    }
+    
+}
+
